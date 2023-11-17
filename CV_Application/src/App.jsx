b2edea3a647ea81a504b9b3bc90f185d76edfc37
DiptoSarkar182 <diptomanchester2018@gmail.com>
2023-11-17 20:03:35 +0600

    footer component added

--- CV_Application/src/App.jsx
+++ CV_Application/src/App.jsx
@@ -1,87 +1,89 @@
-import { useState } from 'react'
-import General_Information from './components/general_information';
-import Educational_Experience from './components/educational_Experience';
-import Practical_Experience from './components/practical_Experience';
-import './styles/App.css';
+import { useState } from "react";
+import General_Information from "./components/general_information";
+import Educational_Experience from "./components/educational_Experience";
+import Practical_Experience from "./components/practical_Experience";
+import Footer from "./components/footer";
+import "./styles/App.css";
 
 export default function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const fullName = firstName + ' ' + lastName;
-  const [schoolName, setSchoolName] = useState(''); 
-  const [titleOfStudy, setTitleOfStudy] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [companyName, setCompanyName] = useState('');
-  const [titleOfPosition, setTitleOfPosition] = useState('');
-  const [responsibilities, setResponsibilities] = useState('');
-  const [expStartDate, setExpStartDate] = useState('');
-  const [expEndDate, setExpEndDate] = useState('');
-  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const fullName = firstName + " " + lastName;
+  const [schoolName, setSchoolName] = useState("");
+  const [titleOfStudy, setTitleOfStudy] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [titleOfPosition, setTitleOfPosition] = useState("");
+  const [responsibilities, setResponsibilities] = useState("");
+  const [expStartDate, setExpStartDate] = useState("");
+  const [expEndDate, setExpEndDate] = useState("");
 
-  function handleFirstName(data){
+  function handleFirstName(data) {
     setFirstName(data);
   }
 
-  function handleLastName(data){
+  function handleLastName(data) {
     setLastName(data);
   }
 
-  function handleEmail(data){
+  function handleEmail(data) {
     setEmail(data);
   }
 
-  function handlePhoneNumber(data){
+  function handlePhoneNumber(data) {
     setPhoneNumber(data);
   }
 
-  function handleSchoolName(data){
+  function handleSchoolName(data) {
     setSchoolName(data);
   }
 
-  function handleTitleOfStudy(data){
+  function handleTitleOfStudy(data) {
     setTitleOfStudy(data);
   }
 
-  function handleStartDate(data){
+  function handleStartDate(data) {
     setStartDate(data);
   }
 
-  function handleEndDate(data){
+  function handleEndDate(data) {
     setEndDate(data);
   }
 
-  function handleCompanyName(data){
+  function handleCompanyName(data) {
     setCompanyName(data);
   }
 
-  function handleTitleOfPosition(data){
+  function handleTitleOfPosition(data) {
     setTitleOfPosition(data);
   }
 
-  function handleResponsibilities(data){
+  function handleResponsibilities(data) {
     setResponsibilities(data);
   }
 
-  function handleExpStartDate(data){
+  function handleExpStartDate(data) {
     setExpStartDate(data);
   }
 
-  function handleExpEndDate(data){
+  function handleExpEndDate(data) {
     setExpEndDate(data);
   }
 
   return (
-    <div className='test'>
-      <div className='headerContainer'>
-        <header className='header'><h1>CV App</h1></header>
+    <div className="test">
+      <div className="headerContainer">
+        <header className="header">
+          <h1>CV App</h1>
+        </header>
       </div>
-      <div className='inputAndOutput'>
-        <div className='input'>
+      <div className="inputAndOutput">
+        <div className="input">
           <div>
-            <General_Information 
+            <General_Information
               sendFirstName={handleFirstName}
               sendLastName={handleLastName}
               sendEmail={handleEmail}
@@ -106,32 +108,30 @@ export default function App() {
             ></Practical_Experience>
           </div>
         </div>
-          <div className='output'>
-            <div className="general_Information">
-              <div>{fullName}</div>
-              <div>{email}</div>
-              <div>{phoneNumber}</div>
-            </div>
+        <div className="output">
+          <div className="general_Information">
+            <div>{fullName}</div>
+            <div>{email}</div>
+            <div>{phoneNumber}</div>
+          </div>
 
-            <div className="educational_Experience">
-              <div>{schoolName}</div>
-              <div>{titleOfStudy}</div>
-              <div>{startDate}</div>
-              <div>{endDate}</div>
-            </div>
+          <div className="educational_Experience">
+            <div>{schoolName}</div>
+            <div>{titleOfStudy}</div>
+            <div>{startDate}</div>
+            <div>{endDate}</div>
+          </div>
 
-            <div className="practical_Experience">
-              <div>{companyName}</div>
-              <div>{titleOfPosition}</div>
-              <div>{responsibilities}</div>
-              <div>{expStartDate}</div>
-              <div>{expEndDate}</div>
-            </div>
+          <div className="practical_Experience">
+            <div>{companyName}</div>
+            <div>{titleOfPosition}</div>
+            <div>{responsibilities}</div>
+            <div>{expStartDate}</div>
+            <div>{expEndDate}</div>
+          </div>
         </div>
       </div>
-      
+      <Footer></Footer>
     </div>
   );
-  
 }
-
